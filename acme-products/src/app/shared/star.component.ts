37eaa3bb0f74152f8a689a2bcc235b713cc3744d
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-12-16 00:24:06 +0530

    Initial Version

---
+++ acme-products/src/app/shared/star.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sv-star',
+  standalone: true,
+  imports: [],
+  templateUrl: './star.component.html',
+  styleUrl: './star.component.scss'
+})
+export class StarComponent {
+
+}
