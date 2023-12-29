f65ae6ee352c763d38a6856c07996411063bd069
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-12-29 12:07:48 +0530

    Product List

--- acme-products/src/app/products/product-list/product-list.component.ts
+++ acme-products/src/app/products/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'sv-product-list',
@@ -8,5 +9,11 @@ import { Component } from '@angular/core';
   styleUrl: './product-list.component.scss'
 })
 export class ProductListComponent {
+  pageTitle = 'Product List';
+  imageWidth = 50;
+  imageMargin = 2;
+  showImage = false;
+  errorMessage = '';
+  sub!: Subscription;
 
 }
