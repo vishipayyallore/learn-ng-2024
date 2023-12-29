1ed0320adbda58c450ee4cc252ef19c3e40b49fe
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-12-29 12:00:46 +0530

    Product List

---
+++ acme-products/src/app/products/product-list/product-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sv-product-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './product-list.component.html',
+  styleUrl: './product-list.component.scss'
+})
+export class ProductListComponent {
+
+}
