59182da2f4e5829e40d98868f56957b97c81e68b
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-12-29 19:59:12 +0530

    Code Refactoring

--- acme-products/src/app/products/product-list/product-list.component.ts
+++ acme-products/src/app/products/product-list/product-list.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { StarComponent } from "../../shared/star/star.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'sv-product-list',
   standalone: true,
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.scss',
-  imports: [StarComponent]
+  imports: [StarComponent, CommonModule]
 })
 export class ProductListComponent {
   pageTitle = 'Product List';
@@ -16,25 +17,27 @@ export class ProductListComponent {
   showImage = false;
   errorMessage = '';
   sub!: Subscription;
-  products: any[] = [{
-    "productId": 2,
-    "productName": "Garden Cart",
-    "productCode": "GDN-0023",
-    "releaseDate": "March 18, 2021",
-    "description": "15 gallon capacity rolling garden cart",
-    "price": 32.99,
-    "starRating": 4.2,
-    "imageUrl": "assets/images/garden_cart.png"
-  },
-  {
-    "productId": 5,
-    "productName": "Hammer",
-    "productCode": "TBX-0048",
-    "releaseDate": "May 21, 2021",
-    "description": "Curved claw steel hammer",
-    "price": 8.9,
-    "starRating": 4.8,
-    "imageUrl": "assets/images/hammer.png"
-  }];
+  products: any[] = [
+    {
+      "productId": 2,
+      "productName": "Garden Cart",
+      "productCode": "GDN-0023",
+      "releaseDate": "March 18, 2021",
+      "description": "15 gallon capacity rolling garden cart",
+      "price": 32.99,
+      "starRating": 4.2,
+      "imageUrl": "assets/images/garden_cart.png"
+    },
+    {
+      "productId": 5,
+      "productName": "Hammer",
+      "productCode": "TBX-0048",
+      "releaseDate": "May 21, 2021",
+      "description": "Curved claw steel hammer",
+      "price": 8.9,
+      "starRating": 4.8,
+      "imageUrl": "assets/images/hammer.png"
+    }
+  ];
 
 }
