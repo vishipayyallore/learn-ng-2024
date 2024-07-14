9afa452e284c2503e09032446d4434e283356aa1
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-07-14 18:05:31 +0530

    Update top-navbar.component.ts

--- coffee-palooza/src/app/components/top-navbar/top-navbar.component.ts
+++ coffee-palooza/src/app/components/top-navbar/top-navbar.component.ts
@@ -2,11 +2,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'sv-top-navbar',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, HttpClientModule],
   templateUrl: './top-navbar.component.html',
   styleUrls: ['./top-navbar.component.css']
 })
