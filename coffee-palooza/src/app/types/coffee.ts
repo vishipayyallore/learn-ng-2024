d1535ef44d0074af478b9af2e35f1f394fd48129
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-07-14 17:44:32 +0530

    Swamy/14jul work (#7)
    * Initial Version
    
    * Changes
    
    * Formatting
    
    * Changes
    
    * Changes
    
    * Changes
    
    * Update styles.css

---
+++ coffee-palooza/src/app/types/coffee.ts
@@ -0,0 +1,21 @@
+export interface Coffee {
+    id: number;
+
+    name: string;
+
+    description: string;
+
+    price: number;
+
+    region: string;
+
+    weight: number;
+
+    roast_level: number;
+
+    flavor_profile: string[];
+
+    grind_option: string[];
+
+    image_url: string;
+}
