5eb8486d438d8af43588ef4c2d986c7ff78a0e49
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-07-18 14:48:01 +0530

    Formatting

--- ng-courses-beginner/src/app/app.component.ts
+++ ng-courses-beginner/src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'ng-courses-beginner';
+  title = 'Ng Courses Beginner';
 }
